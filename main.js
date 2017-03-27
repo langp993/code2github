b2493141f0e2466596dcbc0909ee2be1aa5d4237
langp993 <patrickphillange@gmail.com>
2017-03-26 21:38:05 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var thisbutton = document.getElementById("mybutton");
+var tog = document.getElementById("mycircle");
+
+thisbutton.addEventListener("click", function(){
+	tog.classList.toggle("open");
+});
+
+
+
+
