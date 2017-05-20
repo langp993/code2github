c15641d78991ca59416f850bf6e836b2d57361d5
langp993 <patrickphillange@gmail.com>
2017-05-19 23:05:07 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key:         'Mcr6sS20NHuvKZcAMrEp62mPm',
+  consumer_secret:      'Ad6Xwg82agtjWlyf9B02Cixz8tLeuIwWiJNDtrhKJd3MCicEtZ',
+  access_token:         '717536631364911109-evckBqatpeLBxv2WMYQq4G02BcG1dhl',
+  access_token_secret:  'AWaEOsBR5xWLc3g0Gbz4Fe8JoTCvPWtWI8sHYEVOMo2s0',
+  timeout_ms:           60*1000,
+};
